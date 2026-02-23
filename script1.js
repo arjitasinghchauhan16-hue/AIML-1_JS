5707bf03b9eec79c3263a2392977f7cd06e75af0
Arjita Singh Chauhan <arjitasinghchauhan16-hue@gmail.com>
2026-02-23 15:34:39 +0530

    operations

---
+++ script1.js
@@ -0,0 +1,30 @@
+// var a =10;
+// var b=20;
+// var linebreak= "<br/>";
+
+// document.write("(a==b)=>");
+// result=(a<b);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("(a<b)=>");
+// result=(a<b);
+// document.write(result);
+// document.write(linebreak);
+
+// var count;
+// document.write("Starting loop"+"<br/>");
+// for(count=0;count<10;count++){
+//     document.write("current count:"+count);
+//     document.write("<br/>");
+// }
+// document.write("loop stopped!");
+
+const person={
+    fname:"Arjita",
+    lname:"Singh",
+    age:20,
+}
+for(let x in person){
+    console.log("personal details:"+x+":"+person[x])
+}
