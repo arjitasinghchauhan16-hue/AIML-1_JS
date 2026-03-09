95fde111e4bd5d8bdb0c9b17e0e5cf96339f17ea
Arjita Singh Chauhan <arjitasinghchauhan16-hue@gmail.com>
2026-03-09 15:53:15 +0530

    index2

--- script1.js
+++ script1.js
@@ -20,11 +20,32 @@
 // }
 // document.write("loop stopped!");
 
-const person={
-    fname:"Arjita",
-    lname:"Singh",
-    age:20,
-}
-for(let x in person){
-    console.log("personal details:"+x+":"+person[x])
-}
+// const person={
+//     fname:"Arjita",
+//     lname:"Singh",
+//     age:20,
+// }
+// for(let x in person){
+//     console.log("personal details:"+x+":"+person[x])
+// }
+
+var myinfo = new Array();
+var myinfo1= Array();
+console.log(myinfo,myinfo1);
+
+let collection = [
+    {},
+    [],
+    true,
+    "john",
+    function(){},
+    8976435464694,
+    undefined,
+    null,
+    new String("abc"),
+    new Date(),
+];
+console.log(collection);
+collection.teacherName="John";
+collection.phoneNo=64655644448;
+console.log(collection.length-1);
---
+++ script2.js
@@ -0,0 +1,7 @@
+const reasult = document.getElementById("para");
+reasult.innerHTML="my self";
+reasult.title="myname";
+document.getElementById("myId");
+const result2= document.getElementByClassName("heading");
+result2.innerHTML="hii";
+document.getElementByClassName("myClass");
